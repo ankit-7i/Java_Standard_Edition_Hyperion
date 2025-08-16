7afd21d322b70c48bee789e0d408bd94bf275336
ankit-7i <ankitrout513151979@gmail.com>
2025-08-16 23:25:13 +0530

    Core_Java_Empire

---
+++ src/com/ankit/java/oops_concept/class_relation_in_java/is_a_relation_inheritance/multiple_inheritance/Multiple_Inheritance_Error_Demo.java
@@ -0,0 +1,27 @@
+package com.ankit.java.oops_concept.class_relation_in_java.is_a_relation_inheritance.multiple_inheritance;
+
+class A {
+    public A() {
+        System.out.println("Good Morning");
+    }
+
+}
+class B {
+    public B (){
+        System.out.println("Good Evening ");
+    }
+}
+/*class C extends A,B{
+    public C (){
+        System.out.println("Hi I am Ankit");
+    }
+}
+
+ */
+public class Multiple_Inheritance_Error_Demo {
+
+    public static void main(String[] args) {
+        //C c1 = new C();
+
+    }
+}
