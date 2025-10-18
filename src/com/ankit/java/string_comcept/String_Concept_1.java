8f4916514620084b8151337d581ae6580b7f6ac4
ankit-7i <ankitrout513151979@gmail.com>
2025-10-18 23:40:26 +0530

    Core_Java_Empire

---
+++ src/com/ankit/java/string_comcept/String_Concept_1.java
@@ -0,0 +1,11 @@
+package com.ankit.java.string_comcept;
+
+public class String_Concept_1 {
+
+    String name = "Ankit";
+    String name1 = new String("Ankit");
+
+    public static void main(String[] args) {
+
+    }
+}
