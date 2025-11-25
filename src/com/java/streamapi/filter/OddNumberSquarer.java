dd8f24a896c8ec980750665d74e1b687fb725cce
ankit-7i <ankitrout513151979@gmail.com>
2025-11-25 12:48:12 +0530

    Core_Java_Empire

--- src/com/java/streamapi/filter/OddNumberSquarer.java
+++ src/com/java/streamapi/filter/OddNumberSquarer.java
@@ -1,4 +1,17 @@
 package com.java.streamapi.filter;
 
+import java.util.stream.Stream;
+
 public class OddNumberSquarer {
+    public static void main(String[] args) {
+
+        Stream.iterate(1, n -> n + 1)   // infinite stream of natural numbers
+                .filter(n -> n % 2 != 0)  // keep only odd numbers
+                .map(n -> n * n)          // square them
+                .limit(10)                // take first 10
+                .forEach(System.out::println);
+
+
+    }
+
 }
