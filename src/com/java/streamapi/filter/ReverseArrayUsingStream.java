08f4725e335d2e0543716b826fcbbb1a7a6c7479
ankit-7i <ankitrout513151979@gmail.com>
2025-11-25 12:52:43 +0530

    Core_Java_Empire

--- src/com/java/streamapi/filter/ReverseArrayUsingStream.java
+++ src/com/java/streamapi/filter/ReverseArrayUsingStream.java
@@ -1,4 +1,20 @@
 package com.java.streamapi.filter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ReverseArrayUsingStream {
+    public static void main(String[] args) {
+        int [] arr = {2,3,4,5,6,76,67,89};
+
+        List<Integer> sortedList = Arrays.stream(arr)
+                .boxed()
+                .sorted((a, b) -> b - a)
+                .collect(Collectors.toList());
+
+
+        System.out.println("Output : " + sortedList);
+
+    }
 }
