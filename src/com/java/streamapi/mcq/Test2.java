74a3ddab0aadb1c83e2307bb9ff3004aae9d7447
ankit-7i <ankitrout513151979@gmail.com>
2025-11-27 16:39:24 +0530

    Core_Java_Empire

---
+++ src/com/java/streamapi/mcq/Test2.java
@@ -0,0 +1,17 @@
+package com.java.streamapi.mcq;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Test2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(12);
+        list.add(13);
+        list.add(16);
+        list.add(14);
+        Optional<Integer> max = list.stream().max((x, y) -> x.compareTo(y));
+        System.out.println(max);
+    }
+}
---
+++ src/com/java/streamapi/mcq/Test4.java
@@ -0,0 +1,16 @@
+package com.java.streamapi.mcq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test4 {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("A");
+        al.add("BC");
+        al.add("CDE");
+        al.add("DEFG");
+        List<String> list = al.stream().filter(x -> x.length() >= 2).peek(x -> {}).toList();
+        System.out.println(list);
+    }
+}
