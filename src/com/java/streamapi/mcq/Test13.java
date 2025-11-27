e10abecb840408b6bfc2c242494e6d58848b051d
ankit-7i <ankitrout513151979@gmail.com>
2025-11-27 17:08:00 +0530

    Core_Java_Empire

--- src/com/java/streamapi/mcq/Test13.java
+++ src/com/java/streamapi/mcq/Test13.java
@@ -1,4 +1,13 @@
 package com.java.streamapi.mcq;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Test13 {
-}
+        public static void main(String[] args) {
+            List<String> words = Arrays.asList();
+            String result = words.stream().map(String::toUpperCase).reduce((s1, s2) -> s1 + s2).orElse("NULL");
+            System.out.println(result);
+        }
+    }
+
---
+++ src/com/java/streamapi/mcq/Test14.java
@@ -0,0 +1,16 @@
+package com.java.streamapi.mcq;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test14 {
+
+        public static void main(String[] args) {
+            List<String> al = Arrays.asList("A", "B", "C", "D");
+            List<String> list = al.stream().skip(1).filter(x -> x.startsWith("B")).peek(x -> {}).toList();
+            System.out.println(list);
+
+        }
+    }
+
+
