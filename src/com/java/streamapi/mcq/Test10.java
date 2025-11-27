aa57576f3c9d7819cc56fac5053039e80a60f11d
ankit-7i <ankitrout513151979@gmail.com>
2025-11-27 16:58:08 +0530

    Core_Java_Empire

---
+++ src/com/java/streamapi/mcq/Test10.java
@@ -0,0 +1,13 @@
+package com.java.streamapi.mcq;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test10 {
+        public static void main(String[] args) {
+            List<Integer> list = Arrays.asList(1,2,3,4,5);
+            list.stream().map(k -> k*2+3-k).forEach(k -> System.out.print(k +" "));
+        }
+
+
+}
---
+++ src/com/java/streamapi/mcq/Test11.java
@@ -0,0 +1,16 @@
+package com.java.streamapi.mcq;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test11 {
+
+        public static void main(String[] args) {
+            List<List<String>> list= Arrays.asList(Arrays.asList("1","2","3","4","5"));
+            list.stream().flatMap(x -> list.stream())
+                    .forEach(System.out::print);
+        }
+    }
+
+
+
