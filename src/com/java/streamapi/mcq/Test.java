1d592a0c208c9359dba74ce6e7032166dcde0299
ankit-7i <ankitrout513151979@gmail.com>
2025-11-27 16:25:18 +0530

    Core_Java_Empire

---
+++ src/com/java/streamapi/mcq/Test.java
@@ -0,0 +1,16 @@
+package com.java.streamapi.mcq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(12);
+        list.add(13);
+        list.add(12);
+        list.add(114);
+        List<Integer> list2 = list.stream().distinct().toList();
+        System.out.println(list2);
+    }
+}
