ffd70434cc42bd554ead684f13abbed7be6341b7
ankit-7i <ankitrout513151979@gmail.com>
2025-11-27 17:10:37 +0530

    Core_Java_Empire

---
+++ src/com/java/streamapi/mcq/Test15.java
@@ -0,0 +1,13 @@
+package com.java.streamapi.mcq;
+
+import java.util.List;
+
+public class Test15 {
+
+            public static void main(String[] args) {
+            List<Integer> list = List.of(2, 3, 4, 5, 6);
+            list.stream().takeWhile(x -> x >= 0).map(x -> x * 0 + 2 + 3).peek(System.out::print).toList();
+        }
+    }
+
+
