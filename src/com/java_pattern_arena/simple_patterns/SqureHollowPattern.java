b8cd1ea693556b49dc83d4e0eca0eccc6625ac63
ankit-7i <ankitrout513151979@gmail.com>
2026-02-18 12:31:13 +0530

    Core_Java_Empire

---
+++ src/com/java_pattern_arena/simple_patterns/SqureHollowPattern.java
@@ -0,0 +1,12 @@
+package com.java_pattern_arena.simple_patterns;
+
+import java.util.Scanner;
+
+public class SqureHollowPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number :");
+        int n = sc.nextInt();
+
+    }
+}
