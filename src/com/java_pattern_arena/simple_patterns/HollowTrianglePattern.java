571fb997b45ffa545bd885f9df9d1e59a158d076
ankit-7i <ankitrout513151979@gmail.com>
2026-02-21 11:56:05 +0530

    Core_Java_Empire

--- src/com/java_pattern_arena/simple_patterns/HollowTrianglePattern.java
+++ src/com/java_pattern_arena/simple_patterns/HollowTrianglePattern.java
@@ -3,11 +3,32 @@ package com.java_pattern_arena.simple_patterns;
 import java.util.Scanner;
 
 public class HollowTrianglePattern {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter row number");
         int num = sc.nextInt();
 
+        for(int row=1; row<=num; row++){
 
+            // spaces
+            for(int col=1; col<=num-row; col++){
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int star=1; star<=2*row-1; star++){
+
+                if(star==1 || star==2*row-1 || row==num)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
