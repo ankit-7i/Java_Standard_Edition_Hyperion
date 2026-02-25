99cc861e3117f315cee24b133924654335e65f46
ankit-7i <ankitrout513151979@gmail.com>
2026-02-25 12:24:01 +0530

    Core_Java_Empire

--- src/com/java_logical_program_section/most_popular_category/CheckingPalindrome.java
+++ src/com/java_logical_program_section/most_popular_category/CheckingPalindrome.java
@@ -1,4 +1,32 @@
 package com.java_logical_program_section.most_popular_category;
 
+import java.util.Scanner;
+
 public class CheckingPalindrome {
+
+
+    public static boolean checkingPalindrome(int num){
+        int rev = 0,temp = num;
+        while (temp !=0){
+            int digit = temp %10;
+            rev = rev * 10 +digit;
+            temp/=10;
+        }
+        return rev == num;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a num to check ");
+        int num = sc.nextInt();
+
+        boolean res = checkingPalindrome(num);
+        if(res){
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not Palindrome");
+        }
+
+
+    }
 }
