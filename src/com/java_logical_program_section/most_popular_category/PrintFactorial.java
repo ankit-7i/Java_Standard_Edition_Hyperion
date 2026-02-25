b5b99656e59a2d55800ecc224c6cefd6c87d3fa2
ankit-7i <ankitrout513151979@gmail.com>
2026-02-25 12:08:36 +0530

    Core_Java_Empire

---
+++ src/com/java_logical_program_section/most_popular_category/PrintFactorial.java
@@ -0,0 +1,24 @@
+package com.java_logical_program_section.most_popular_category;
+
+import java.util.Scanner;
+
+public class PrintFactorial {
+    public static long printFactorial(long num){
+        long val = 1;
+        for(long i = 1; i<= num;i++){
+            val*=i;
+        }
+        return val;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter Value ");
+        long val = sc.nextLong();
+
+        long res = printFactorial(val);
+
+        System.out.println(res);
+
+    }
+}
---
+++ src/com/java_logical_program_section/most_popular_category/Reverse_A_Number.java
@@ -0,0 +1,5 @@
+package com.java_logical_program_section.most_popular_category;
+
+public class Reverse_A_Number {
+
+}
