d7beb6bef6d79628827420437033d5897971956d
ankit-7i <ankitrout513151979@gmail.com>
2026-02-25 12:13:56 +0530

    Core_Java_Empire

--- src/com/java_logical_program_section/most_popular_category/Reverse_A_Number.java
+++ src/com/java_logical_program_section/most_popular_category/Reverse_A_Number.java
@@ -1,5 +1,29 @@
 package com.java_logical_program_section.most_popular_category;
 
+import java.util.Scanner;
+
 public class Reverse_A_Number {
 
+    public static int reverseA_Number(int num){
+        int rev =0 ;
+
+        while (num != 0){
+            int digit = num%10;
+            rev = rev* 10 +digit;
+            num/=10;
+        }
+
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("Enter a num to reverse ");
+        int num = sc.nextInt();
+
+        int res = reverseA_Number(num);
+
+        System.out.println(res);
+    }
 }
