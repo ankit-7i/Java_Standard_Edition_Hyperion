8c5e15231e494eb4876babf99213440c451635f7
ankit-7i <ankitrout513151979@gmail.com>
2026-02-26 10:50:54 +0530

    Core_Java_Empire

--- src/com/java_pattern_arena/simple_patterns/BlockDiamondPattern.java
+++ src/com/java_pattern_arena/simple_patterns/BlockDiamondPattern.java
@@ -1,4 +1,33 @@
 package com.java_pattern_arena.simple_patterns;
 
+import java.util.Scanner;
+
 public class BlockDiamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows ");
+        int n = sc.nextInt();
+
+        // upper part of diamond
+        for(int i =1; i<=n;i++){
+            for (int j = 1; j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<= (2*i-1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Lower Part of diamond
+        for (int i = n-1;i>=1;i--){
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= (2 * i - 1); j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+        sc.close();
+    }
 }
