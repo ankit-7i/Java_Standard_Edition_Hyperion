c0b25a60b8fd9931b22b8d66383d136952984bc3
ankit-7i <ankitrout513151979@gmail.com>
2026-03-05 12:28:36 +0530

    Core_Java_Empire

--- src/com/java_logical_program_section/array_logical_programs/ReversingAnArray.java
+++ src/com/java_logical_program_section/array_logical_programs/ReversingAnArray.java
@@ -1,7 +1,29 @@
 package com.java_logical_program_section.array_logical_programs;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ReversingAnArray {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < n; i++) {
+             arr[i] =  sc.nextInt();
+        }
+        System.out.println("Original Array"+ Arrays.toString(arr));
 
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println("Reversed Array"+ Arrays.toString(arr));
     }
 }
