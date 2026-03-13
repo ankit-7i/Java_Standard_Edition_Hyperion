5cbd63439e33dca48d006b0f40298c8a95bc4d75
ankit-7i <ankitrout513151979@gmail.com>
2026-03-13 11:40:31 +0530

    Core_Java_Empire

--- src/com/java_logical_program_section/string_logical_programs/StringContainsOnlyDigitRegxStyle.java
+++ src/com/java_logical_program_section/string_logical_programs/StringContainsOnlyDigitRegxStyle.java
@@ -1,4 +1,18 @@
 package com.java_logical_program_section.string_logical_programs;
 
+import java.util.Scanner;
+
 public class StringContainsOnlyDigitRegxStyle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string");
+        String str = sc.nextLine();
+
+        if(str.matches("\\d+")){
+            System.out.println("The string contains only digits");
+        }
+        else{
+            System.out.println("The string does not contains only digits");
+        }
+    }
 }
